44f71e3e9ad667066ed0ee6ffd1a319119b32010
GoktanBattal <goktanbattal@gmail.com>
2022-11-17 13:26:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
+import styled from "styled-components";
 
+const BodyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  font-size: 20px;
+  width: 100wv;
+  height: 100vh;
+`;
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [loading, setLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      setLoading(true);
+      axios.get('https://swapi.dev/api/people/').then((response) => {
+        const data = response.data;
+        console.log('date:',data);
+        if (data && data.length > 0) {
+          setCharacters(data);
+        }
+        setLoading(false);
+      }).catch((error) => {
+        setLoading(false);
+        console.error('Error: ', error);
+      });
+    }
+
+    getData();
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading && characters && characters.map((character, index) => <BodyContainer><Character info={character} key={index}/></BodyContainer>)}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const BodyContainer = styled.div`
+    display: flex;
+    flex-wrap: no-wrap;
+    justify-content: space-between;
+    margin: 10px;
+    padding: 5px 10px;
+    background: #d4c6bd;
+    width: 50%;
+    min-width: 350px;
+    border-radius: 8px;
+`;
+const NameContent = styled.div`
+    color: #4e2f1c;
+    font-size: 24px;
+    font-family: 'Unlock', cursive;
+`;
+
+const BirthYearContent = styled.div`
+    background-color: #5a5a5a;
+    border-top-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+    border-top-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    padding: 4px 10px;
+`;
+const BirthInsideContent = styled.div`
+    color: #d4c6bd;
+    font-size: 15px;
+    font-family: 'Unlock', cursive;
+`;
+
+const Character = (props) => {
+    return (
+        <BodyContainer>
+            <NameContent>{props.info.name ? props.info.name : ''}</NameContent>
+            <BirthYearContent>
+                <BirthInsideContent>{props.info.birth_year ? props.info.birth_year : ''}</BirthInsideContent>
+            </BirthYearContent>
+        </BodyContainer>
+    );
+}
+
+export default Character;
